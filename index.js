753b41b6e6090b640f614e13e66ebd1698b628ae
mdesury <127192048+mdesury@users.noreply.github.com>
2023-06-06 19:03:40 +0200

    1 file modified

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 // Import stylesheets
 import './style.css';
 
-document.getElementById("demo").innerHTML = "Primo<br>";
-document.getElementById("demo").innerHTML += "Secondo<br>";
-document.getElementById("demo").innerHTML += "<b>Terzo</b>";
+let a = 25;
+let b = 27;
+
+if (a == b) {
+  document.getElementById("demo").innerHTML = "Sono gemelli";
+} else if (a > b) {
+  document.getElementById("demo").innerHTML += "Remo è il fratello più grande";
+} else if (a < b) {
+  document.getElementById("demo").innerHTML += "Nino è il fratello più grande";
+}
+
+
