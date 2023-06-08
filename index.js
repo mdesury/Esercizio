ae6dad43bfc1002ce1f9514950e3fff4936d882c
mdesury <127192048+mdesury@users.noreply.github.com>
2023-06-08 17:36:16 +0200

    1 file modified

--- index.js
+++ index.js
@@ -7,13 +7,21 @@ const libri = [
   { autore: "JK Rowling", titolo: "Harry Potter e la pietra filosofale" }
 ];
 
-const stringa = "Tolkien";
+const stringa = "";
+libriCorrispondenti = libri.filter(function(libro) {
+  return libro.titolo.toLowerCase().includes(stringa);
+})
 
-libri.forEach(function(libro) {
+libri.forEach(function(libro)) {
   if (libro.titolo.toLowerCase().includes(stringa.toLowerCase())) {
-  console.log("Il titolo del libro è", libro.titolo, "scritto da", libro.autore);
+  console.log("l'autore del libro che cerchi è:", libro.autore);
   }
-});
+  });
+  
+
+
+
+
 
 
 
