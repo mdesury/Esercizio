2b82c5eaf6f112c92a9a28a97ed614a78077535b
mdesury <127192048+mdesury@users.noreply.github.com>
2023-06-08 12:48:38 +0200

    1 file modified

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 // Import stylesheets
 import './style.css';
 
-let titolo = "Percy Jackson";
-let autore = "Rick Riordan";
-let stringa = window.prompt("Inserisci una parola:");
+const libri = [
+  { autore: "Mario Rossi", titolo: "Il grande Gatsby" },
+  { autore: "Luca Bianchi", titolo: "L'ombra del vento" },
+  { autore: "Giulia Verdi", titolo: "Il signore degli anelli" },
+  { autore: "Mario Rossi", titolo: "Harry Potter e la pietra filosofale" }
+];
 
-let risultato = (titolo.toLowerCase() + autore.toLowerCase()).includes(stringa);
-console.log(risultato)
+const sequenzaCaratteri = "grande";
+
+libri.forEach(function(libro) {
+  if (libro.titolo.toLowerCase().includes(sequenzaCaratteri.toLowerCase())) {
+  console.log("Il tito del libro è", libro.titolo, "scritto da", libro.autore);
+  }
+});
+
+
 
