b418d773eea7b039c364c2759998855496f2a5ad
mdesury <127192048+mdesury@users.noreply.github.com>
2023-06-10 18:02:14 +0200

    1 file modified

--- index.js
+++ index.js
@@ -4,16 +4,25 @@ import './style.css';
 const archivio = [
   {titolo: "Harry Potter", autore: "JK Rowling"},
   {titolo: "Percy Jackson", autore: "Rick Riordan"},
-  {titolo: "Il Signore degli Anelli", autore: "JRR Tolkien"}]
+  {titolo: "Il Signore degli Anelli", autore: "JK Tolkien"}]
 
-const stringa = "Percy";
-const corrispondenze = 0;
+const stringa = "percy";
+let corrispondenze = 0;
+let autore = ""
 
 archivio.forEach(function(libro) {
 if (libro.titolo.toLowerCase().includes(stringa)) {
-console.log("Il libro è stato scritto da ", libro.autore)
+  corrispondenze ++;
+  autore = libro.autore;
+  console.log("Numero di corrispondenze: ", corrispondenze)
 }
 })
+
+if (corrispondenze === 1) {
+  console.log("Il libro è stato scritto da ", autore)
+} else {
+  console.log("non sono state trovate corrispondenze")
+}
 
 
 
