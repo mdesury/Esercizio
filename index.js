15c643eb3b81a77a0e63f7f66f4ea21c7a47f6bb
mdesury <127192048+mdesury@users.noreply.github.com>
2023-06-10 17:34:59 +0200

    1 file modified

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 // Import stylesheets
 import './style.css';
 
-const titolo = "Harry Potter";
-const autore = "JK Rowling";
-const stringa = "";
+const archivio = [
+  {titolo: "Harry Potter", autore: "JK Rowling"},
+  {titolo: "Percy Jackson", autore: "Rick Riordan"},
+  {titolo: "Il Signore degli Anelli", autore: "JRR Tolkien"}]
 
-const risultato = (titolo.toLowerCase() + autore.toLowerCase()).includes(stringa)
-console.log(risultato)
+const stringa = "Percy";
+const corrispondenze = 0;
 
+archivio.forEach(function(libro) {
+if (libro.titolo.toLowerCase().includes(stringa)) {
+console.log("Il libro è stato scritto da ", libro.autore)
+}
+})
 
 
 
