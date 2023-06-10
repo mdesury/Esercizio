285c04b9f0c180ffffba86495d9e5bb5111ae68d
mdesury <127192048+mdesury@users.noreply.github.com>
2023-06-10 15:32:40 +0200

    1 file modified

--- index.js
+++ index.js
@@ -1,23 +1,12 @@
 // Import stylesheets
 import './style.css';
 
-const libri = [
-  { autore: "Rick Riordan", titolo: "Percy Jackson" },
-  { autore: "JR Tolkien", titolo: "Il signore degli anelli" },
-  { autore: "JK Rowling", titolo: "Harry Potter e la pietra filosofale" }
-];
-
+const titolo = "Harry Potter";
+const autore = "JK Rowling";
 const stringa = "";
-libriCorrispondenti = libri.filter(function(libro) {
-  return libro.titolo.toLowerCase().includes(stringa);
-})
 
-libri.forEach(function(libro)) {
-  if (libro.titolo.toLowerCase().includes(stringa.toLowerCase())) {
-  console.log("l'autore del libro che cerchi è:", libro.autore);
-  }
-  });
-  
+const risultato = (titolo.toLowerCase() + autore.toLowerCase()).includes(stringa)
+console.log(risultato)
 
 
 
